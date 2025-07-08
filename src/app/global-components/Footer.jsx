99ac0b33f84e888264f8763fd314642884fd8ce5
pchound <chaonerdguy@gmail.com>
2025-07-07 18:03:57 -0600

    contact-footer

--- src/app/global-components/Footer.jsx
+++ src/app/global-components/Footer.jsx
@@ -1,12 +1,25 @@
 'use client';
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
-        <p className="text-md text-gray-500 text-center pt-7 pb-7">
-            &copy; {currentYear} All rights reserved
-        </p>
+        <div>
+
+            <p className="text-md text-gray-500 text-center">
+                (123) 456-7890
+            </p>
+
+            <p className="text-md text-gray-500 text-center">
+                123 Maple Street. Anytown, PA 17101
+            </p>
+
+
+            <p className="text-sm text-gray-500 text-center pt-4 pb-2">
+                &copy; {currentYear} All rights reserved
+            </p>
+
+        </div>
     );
 };
 
