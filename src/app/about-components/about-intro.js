f3be90be883ab58eae0333d26d2b69c646201dc3
pchound <chaonerdguy@gmail.com>
2025-09-20 12:19:28 -0600

    about-upgrade

--- src/app/about-components/about-intro.js
+++ src/app/about-components/about-intro.js
@@ -36,9 +36,44 @@ const AboutIntro = () => {
                     }}
                 >
                    Our mission is to provide compassionate, whole-person, and inclusive mental health care that blends evidence-based medicine with natural approaches. We are dedicated to expanding healing for Utah’s veterans, families, and communities most impacted by social determinants of health—such as  addiction and mental wellbeing stigma—through personalized, life-changing treatment that restores hope, fosters resilience, and delivers innovative care in a supportive and healing environment. 
-
-
                 </p>
+
+<br></br><br></br>
+
+
+            <h1 className="text-[#88695e] text-4xl font-bold drop-shadow-xl mb-4 text-center"
+                style={{
+                    fontFamily: '"Quicksand", sans-serif',
+                    fontWeight: 300
+                }}
+            >
+                How I help.
+            </h1>
+
+
+                <p className='text-[#0f0b0a]'
+                    style={{
+                        fontFamily: '"Quicksand", sans-serif',
+                        fontWeight: 300
+                    }}
+                >
+                    
+                   As a Psychiatric-Mental Health Nurse Practitioner, I provide medication management plus non-pharmacologic supports (sleep, nutrition, movement, school collaboration). I see children, teens, and adults and offer a respectful, faith-friendly, inclusive space for all families.
+                </p>
+                    <br></br>
+
+                    
+                <p className='text-[#0f0b0a] text-center'
+                    style={{
+                        fontFamily: '"Quicksand", sans-serif',
+                        fontWeight: 300
+                    }}
+                > Call to book:<br></br> (801) 796-2552
+                <br></br>
+                or book online <a href="/contact" className="text-blue-500 underline">here</a>.
+                </p>
+                
+               
             </div>
         </div>
     );
