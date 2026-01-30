5e4febed3a1654ac7246607c7791e7c44351e992
pchound <chaonerdguy@gmail.com>
2026-01-30 08:59:44 -0700

    google-tag-added

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5G3JWTSK');
+          `}
+        </Script>
+          {/*End Google Tag Manager*/}
+
         {/* Favicon */}
         <link rel="icon" href="/logo.png" />
 
@@ -42,8 +58,18 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5G3JWTSK"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
 
-<script src="//code.tidio.co/epnjvabie0mjl8vkuqgtcz8zgmbjzfr1.js" async></script>
+        <Script src="//code.tidio.co/epnjvabie0mjl8vkuqgtcz8zgmbjzfr1.js" strategy="afterInteractive" />
         {children}
       </body>
     </html>
